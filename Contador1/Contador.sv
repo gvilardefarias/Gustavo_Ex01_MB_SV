4d38e577e8944ca785e2c8df38e8f15f12364ef6
Gustavo Vilar <gvilardefarias@ymail.com>
2016-11-22 16:06:31 -0300

    Ajustes no 1° Contador

--- Contador1/Contador.sv
+++ Contador1/Contador.sv
@@ -1,18 +1,21 @@
 module Contador1(input logic clk, rst, output logic[3:0] s);
     logic direction;
 
-    always @(posedge clk or posedge rst)
+    always_ff @(posedge clk or posedge rst)
         if(rst) begin
-            s = 4'd0;
-            direction = 1'b0;
+            s <= 4'd0;
+            direction <= 1'b0;
         end
         else begin
-            if(direction)
-                s = s - 4'd1;
-            else
-                s = s + 4'd1;
+          if(direction || s==4'd15)
+                s <= s - 4'd1;
+         else
+           if(!direction || s==4'd0)
+                s <= s + 4'd1;
 
-            if(s==4'd15 || s==4'd0)
-                direction = ~direction;
+          if(s==4'd15 && !direction)
+                direction <= 1'b1;
+          if(s==4'd0 && direction)
+                direction <= 1'b0;
         end
 endmodule
